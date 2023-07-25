8212fd7605185389ccae000807d2366923dd6550
krisograbek <krzysztof.ograbek@gmail.com>
2023-07-25 13:35:23 +0200

    style adjusments and minor bugfixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,56 @@ import GlobalStyle from './globalStyles';
 
 const TextArea = styled.textarea`
   width: 100%;
-  height: 100px;
+  height: 150px;
   margin-bottom: 20px;
+  padding: 10px;
+  font-size: 16px;
+  background-color: #222;
+  color: white;
 `;
+
+const SubmitButton = styled.button`
+  margin-bottom: 20px;
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: none;
+  background-color: #569cd6;
+  color: white;
+  cursor: pointer;
+  &:hover {
+    background-color: #4a86c5;
+  }
+`;
+
+const Loader = styled.div`
+  margin: 30px auto;
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 24px;
+  height: 24px;
+  animation: spin 2s linear infinite;
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+`;
+
+const AnimatedArea = styled.div`
+  width: 80%;
+  height: calc(100vh - 300px); /* Full height minus 20px top and bottom margin */
+  margin: 20px auto; /* Vertical margin: 20px, Horizontal margin: auto */
+  background: #282a36;
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, .2); 
+  border-radius: 5px;
+  padding: 20px;
+  overflow: auto; /* Add scrollbar if content is too tall */
+  box-sizing: border-box; /* Include padding and border in element's total width and height */
+  color: #f8f8f2;
+`;
+
 
 const type = keyframes`
   0% {width: 0;}
@@ -73,28 +120,43 @@ const CommentSpan = styled.span`
 // variable: #9DDDFC
 
 
-
 function App() {
   const [code, setCode] = useState('');
   const [animatedCode, setAnimatedCode] = useState(''); // new state variable
+  const [loading, setLoading] = useState(false); // new state variable
+  const [animatedHeight, setAnimatedHeight] = useState('auto');
 
-  const handleChange = async (e) => {
-    setCode(e.target.value);
 
+  const handleSubmit = async () => {
+    setLoading(true);
     try {
-      const response = await axios.post('http://localhost:5000/convert', { code: e.target.value });
-      setAnimatedCode(response.data.html); // set animatedCode when OpenAI response is received
-      console.log(response.data.html)
+      const response = await axios.post('http://localhost:5000/convert', { code });
+      let html = response.data.html;
+      html = html.replace(/```html|```/g, ""); // remove ```html and ```
+
+      // Count the number of lines in the code
+      const lineCount = (code.match(/\n/g) || '').length + 1;
+      // Set the height of the animated window (you might need to adjust the multiplier)
+      setAnimatedHeight(lineCount * 1.5 + 'em');
+
+      setAnimatedCode(html);
+      console.log(html)
     } catch (error) {
       console.error('Error during conversion:', error);
     }
+    setLoading(false);
+  }
+
+
+  const handleChange = (e) => {
+    setCode(e.target.value);
   }
 
   let totalDuration = 0;
 
   const generateAnimation = (text, index, indent) => {
     const lengthMultiplier = text.length > 35 ? 0.65 : 0.7;
-    const width = (parseInt(Math.floor(text.length * lengthMultiplier)) + parseInt(indent)).toString();
+    const width = (parseInt(Math.ceil(text.length * lengthMultiplier)) + parseInt(indent)).toString();
     // console.log("Width", width)
     // console.log("Width + ind", width + indent)
     const duration = width / 16; /* Adjust this to speed up or slow down the animation */
@@ -177,8 +239,16 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <TextArea value={code} onChange={handleChange} />
-      {lines}
+      <TextArea
+        value={code}
+        onChange={handleChange}
+        placeholder="Paste code here"
+      />
+      <SubmitButton onClick={handleSubmit}>Submit</SubmitButton>
+      {loading ? <Loader /> : null}
+      <AnimatedArea height={animatedHeight}>
+        {loading ? <p>Loading...</p> : lines}
+      </AnimatedArea>
     </div>
   );
 }
--- frontend/src/globalStyles.js
+++ frontend/src/globalStyles.js
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    padding: 40px;
-    background-color: #121212;
-  }
+body {
+  background-color: #f5f5f5;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Arial, sans-serif;
+}
 `
 export default GlobalStyle;
